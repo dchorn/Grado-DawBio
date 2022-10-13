b3182ab9d392ded025c4fecfc3b9bb76e3a63b12
Denys Chorny <denis.man.07@gmail.com>
2022-10-13 18:55:22 +0200

    asociative array from the file

--- PHP/ExercicisFitxers/ex1.php
+++ PHP/ExercicisFitxers/ex1.php
@@ -1,1 +1,24 @@
-Programa que valida nom d'usuari i paraula de pas. Les dades estan en un fitxer.
+<?php
+	//Programa que valida nom d'usuari i paraula de pas. Les dades estan en un fitxer.
+    // Abrir el archivo en modo de sólo lectura:
+    $archivo = fopen("ex1.txt","rb");
+	
+	$array = [];
+	$dict = [];
+	$counter = 0;
+	
+	if( $archivo == false ) {
+		echo "Error al abrir el archivo";
+	} else {
+		while (feof($archivo) == false ){
+			array_push($array, fgets($archivo));
+			array_push($dict,preg_split("/\:/", $array[$counter]));
+			$aa[$counter]= array('user' => $dict[$counter][0], 'passwd' => $dict[$counter][1]); 
+			$counter++;
+		}
+	}
+
+	print_r($aa);	
+	fclose($archivo);
+
+?>
