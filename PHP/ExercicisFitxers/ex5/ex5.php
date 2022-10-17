0d77809488edef7fee2dddccce312a5d86955014
Denys Chorny <denis.man.07@gmail.com>
2022-10-17 19:21:14 +0200

    Started the project

--- PHP/ExercicisFitxers/ex5/ex5.php
+++ PHP/ExercicisFitxers/ex5/ex5.php
@@ -1,1 +1,6 @@
-Es desa en fitxer informació sobre paísos (nom, capital, extensió, població, etc). Amb un selector se selecciona un país i es mostra la seva informació en un formulari. El formulari permetrà enviar canvis en les dades i desar-les al fitxer.
+<?php
+// Es desa en fitxer informació sobre paísos (nom, capital, extensió, població, etc). Amb un selector se selecciona un país 
+// i  es mostra la seva informació en un formulari. El formulari permetrà enviar canvis en les dades i desar-les al fitxer.
+
+
+?>
