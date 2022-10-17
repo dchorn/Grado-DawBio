7c2b460537ec98fabce55c12f87040cecfa40fd3
Denys Chorny <denis.man.07@gmail.com>
2022-10-17 19:37:42 +0200

    change a str from the databaso to another str

--- PHP/ExercicisFitxers/ex5/ex5.php
+++ PHP/ExercicisFitxers/ex5/ex5.php
@@ -2,5 +2,13 @@
 // Es desa en fitxer informació sobre paísos (nom, capital, extensió, població, etc). Amb un selector se selecciona un país 
 // i  es mostra la seva informació en un formulari. El formulari permetrà enviar canvis en les dades i desar-les al fitxer.
 
+$fname = "db/country.csv";
+$fhandle = fopen($fname,"r");
+$content = fread($fhandle,filesize($fname));
+$content = str_replace("spain", "spain", $content);
+
+$fhandle = fopen($fname,"w");
+fwrite($fhandle,$content);
+fclose($fhandle);
 
 ?>
