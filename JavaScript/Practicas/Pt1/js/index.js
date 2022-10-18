4128ee96dcffa00f7d330fd4dea74d7eec8f8a8d
Denys Chorny <denis.man.07@gmail.com>
2022-10-18 19:22:35 +0200

    fixed login, now it displays onload with page

--- JavaScript/Practicas/Pt1/js/index.js
+++ JavaScript/Practicas/Pt1/js/index.js
@@ -1,6 +1,6 @@
 addEventListener('DOMContentLoaded', function() {
 
-    document.getElementById("login").style.display="none";
+    document.getElementById("login").style.display="block";
     document.getElementById("register").style.display="none";
 
 // Funcion que pone display:block o display:none dependiendo del boton
