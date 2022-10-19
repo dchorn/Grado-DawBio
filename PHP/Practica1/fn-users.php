62ac277c9dfe409a3ca7f59fcb1673d966e231fa
Denys Chorny <denis.man.07@gmail.com>
2022-10-19 13:02:45 +0200

    PHP template for the exercice

---
+++ PHP/Practica1/fn-users.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * search a username in the data base
+ * @param string $username
+ * @return array with all data of that user or ... if not found
+ * */
+function searchUser(string $username): array {
+//TODO
+}
+
+/**
+ * insert a new user in file, preventing duplicates
+ * @param string $username
+ * @param string $password
+ * @param string $role
+ * @param string $name
+ * @param string $surname
+ * @return bool true if successfully inserted, false otherwise
+ * */
+function insertUser(
+	string $username,
+	string $password,
+	string $role,
+	string $name,
+	string $surname): bool {
+	//TODO
+}
+
+
+?>
