ae2976cf0eac4f55cdfecb60f4c61fbb207d050c
Denys Chorny <denis.man.07@gmail.com>
2022-10-19 20:11:48 +0200

    Send register form to html

--- JavaScript/Practicas/Pt1/js/index.js
+++ JavaScript/Practicas/Pt1/js/index.js
@@ -13,6 +13,39 @@ addEventListener('DOMContentLoaded', function() {
 				document.getElementById("register").style.display = "block"
 			})
 		}
+
+	// Register to PHP
+    document.getElementById("register-submit").addEventListener("click", function() {
+        var user   = document.getElementById("register-uname").value;    
+        var passwd = document.getElementById("register-psw").value;
+        var  name  = document.getElementById("register-name").value;
+        let registerUser = { // objeto js
+            usuari: user,
+            contra: passwd,
+			nom: name
+        };
+
+	//enviar aquest objecte al servidor:
+		let xhr = new XMLHttpRequest();
+		xhr.open("POST", "./php/server.php");//obrir conexio 
+
+		xhr.send(JSON.stringify(registerUser));//enviament de dades
+		
+
+		xhr.onload=function() { //esperar a rebre les dades
+			if (xhr.status !=200) { // analiza el estado http
+				alert(`ERROR! ${xhr.status}: ${xhr.statusText}`); // ej. 404: No encontrado
+			} else {
+				// xhr.response es un JSON que viene des de PHP.
+				let responseServer = JSON.parse(xhr.response); // reconvertir la respuesta/ parsearla
+				document.getElementById("response").innerHTML=responseServer;
+			};
+		};
+
+	});
+
+
+	// Login to PHP
     document.getElementById("login-submit").addEventListener("click", function() {
         var name   = document.getElementById("login-uname").value;    
         var passwd = document.getElementById("login-psw").value;
@@ -20,13 +53,14 @@ addEventListener('DOMContentLoaded', function() {
             nom: name,
             contra: passwd
         };
-        console.log(user);
-
 
 	//enviar aquest objecte al servidor:
 		let xhr = new XMLHttpRequest();
-		xhr.open("POST", "./php/server.php");//obrir conexio
+		xhr.open("POST", "./php/server.php");//obrir conexio 
+
 		xhr.send(JSON.stringify(user));//enviament de dades
+		
+
 		xhr.onload=function() { //esperar a rebre les dades
 			if (xhr.status !=200) { // analiza el estado http
 				alert(`ERROR! ${xhr.status}: ${xhr.statusText}`); // ej. 404: No encontrado
