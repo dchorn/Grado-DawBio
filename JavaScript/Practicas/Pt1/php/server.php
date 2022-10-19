619d78b20f3388f5fe464db831598b51e503c114
Denys Chorny <denis.man.07@gmail.com>
2022-10-19 12:55:57 +0200

    get data from js, check if the data is in the db, send information

--- JavaScript/Practicas/Pt1/php/server.php
+++ JavaScript/Practicas/Pt1/php/server.php
@@ -1,21 +1,47 @@
-
 <?php
-
     // Recoger peticiones
     $entry = file_get_contents('php://input');
 
+	define('FIELDS', ['password', 'role', 'name']);
+
+
+	function csvToArr(): array {
+	$rows = array_map('str_getcsv', file('./db/users.csv'));	
+    $csv    = array();
+    foreach($rows as $row) {
+        $csv[$row[0]] = [FIELDS[0] => $row[1], FIELDS[1] => $row[2], FIELDS[2] => $row[3]];
+	}
+	return $csv;
+	}
+
+	//$csv = csvToArr();
+	//echo '<pre>';
+	//print_r($csv);
+	//echo '</pre>';
+	
+	function searchUser(string $username, string $password): string {
+	$users = csvToArr();
+	if (array_key_exists($username, $users)) {
+		if ($users[$username]["password"] == $password) {
+			$ret_value = json_encode([$username, $users[$username]["role"]]);
+		} else {
+			$ret_value = json_encode("Password does not match");
+		}
+	} else {
+		$ret_value = json_encode("Username not found");
+	};
+	return $ret_value;
+}
+	
     //paso de JSON, cadena de texto, a variable de PHP
     $entrada = json_decode($entry);
 
-
     $name   = $entrada->{'nom'}; 
     $passwd = $entrada->{'contra'};
-
-	$array = [$name, $passwd];
 
     //codigo de PHP hago lo que sea y al final necesito
     //enviar el siguiente array
 
     //envio del resultado imprimiendolo: variable PHP a JSON
-	echo json_encode($array);
+	echo searchUser($name, $passwd);
 ?>
