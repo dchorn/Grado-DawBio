6285798a0e9cbfba848ef0197e37ad4ba2263630
Denys Chorny <denis.man.07@gmail.com>
2022-10-26 16:24:43 +0200

    Function that creates creates a user and save it into the db

--- JavaScript/Practicas/Pt1/php/server.php
+++ JavaScript/Practicas/Pt1/php/server.php
@@ -27,21 +27,31 @@
 	return $ret_value;
 	}
 
-	function createUser(string $username, string $password, string $name, string $role="user") {
-	$fp = fopen('file.csv', 'a');
-	fputcsv($fp, [$username,$password,$role,$name]);
-	fclose($fp);
-}
+	function createUser($file) {
+		
+		$fp = fopen($file,'a');
+		var_dump($fp);
+		fputcsv($fp, ["aaaa","aaaa","aaaa","aaaa"], ",");
+		fclose($fp);
+	}
 	
     //paso de JSON, cadena de texto, a variable de PHP
     $entrada = json_decode($entry);
 
-    $name   = $entrada->{'nom'}; 
-    $passwd = $entrada->{'contra'};
+	//$name   = $entrada->{'nom'};
+	//$username = $entrada->{'usuari'};
+    //$password = $entrada->{'contra'};
+
+
+	$file = './db/users.csv';
+	createUser($file);
+
+	//echo json_encode("working");
 
     //codigo de PHP hago lo que sea y al final necesito
     //enviar el siguiente array
 
     //envio del resultado imprimiendolo: variable PHP a JSON
-	echo searchUser($name, $passwd);
+	
+	//echo searchUser($name, $password);
 ?>
