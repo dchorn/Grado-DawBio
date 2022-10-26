f142057c206cf07f63d9f91a4949d7aebc2a9525
Denys Chorny <denis.man.07@gmail.com>
2022-10-26 17:20:30 +0200

    Get data from login and register, checks the operator and do 1 of the two actions

--- JavaScript/Practicas/Pt1/php/server.php
+++ JavaScript/Practicas/Pt1/php/server.php
@@ -1,57 +1,61 @@
 <?php
-    // Recoger peticiones
-    $entry = file_get_contents('php://input');
+// Recoger peticiones
+$entry = file_get_contents('php://input');
 
-	define('FIELDS', ['password', 'role', 'name']);
+define('FIELDS', ['password', 'role', 'name']);
+define('DB_FILE', './db/users.csv');
 
-	function csvToArr(): array {
-	$rows = array_map('str_getcsv', file('./db/users.csv'));	
-    $csv    = array();
-    foreach($rows as $row) {
+function csvToArr(): array
+{
+    $rows = array_map('str_getcsv', file(DB_FILE));
+    $csv = array();
+    foreach ($rows as $row) {
         $csv[$row[0]] = [FIELDS[0] => $row[1], FIELDS[1] => $row[2], FIELDS[2] => $row[3]];
-	}
-	return $csv;
-	}
+    }
+    return $csv;
+}
 
-	function searchUser(string $username, string $password): string {
-	$users = csvToArr();
-	if (array_key_exists($username, $users)) {
-		if ($users[$username]["password"] == $password) {
-			$ret_value = json_encode([$username, $users[$username]["role"]]);
-		} else {
-			$ret_value = json_encode("Password does not match");
-		}
-	} else {
-		$ret_value = json_encode("Username not found");
-	};
-	return $ret_value;
-	}
+function searchUser(string $username, string $password): string
+{
+    $users = csvToArr();
+    if (array_key_exists($username, $users)) {
+        if ($users[$username]["password"] == $password) {
+            $ret_value = json_encode([$username, $users[$username]["role"]]);
+        } else {
+            $ret_value = json_encode("Password does not match");
+        }
+    } else {
+        $ret_value = json_encode("Username not found");
+    };
+    return $ret_value;
+}
 
-	function createUser($file) {
-		
-		$fp = fopen($file,'a');
-		var_dump($fp);
-		fputcsv($fp, ["aaaa","aaaa","aaaa","aaaa"], ",");
-		fclose($fp);
-	}
-	
-    //paso de JSON, cadena de texto, a variable de PHP
-    $entrada = json_decode($entry);
+function createUser($username, $password, $rol, $name)
+{
+    $fp = fopen(DB_FILE, 'a');
+    var_dump($fp);
+    fputcsv($fp, [$username, $password, $rol, $name], ",");
+    fclose($fp);
+}
 
-	//$name   = $entrada->{'nom'};
-	//$username = $entrada->{'usuari'};
-    //$password = $entrada->{'contra'};
+//paso de JSON, cadena de texto, a variable de PHP
+
+$entrada = json_decode($entry, true);
+
+$operator = $entrada['operator'];
+
+if ($operator === 'login') {
+	echo searchUser($entrada['username'], $entrada['password']);
+}
+elseif($operator === 'register') {
+	createUser($entrada['username'],$entrada['password'],'client',$entrada['name']);
+	echo "User Created";
+}
+
+//$name   = $entrada->{'nom'};
+//$username = $entrada->{'usuari'};
+//$password = $entrada->{'contra'};
 
 
-	$file = './db/users.csv';
-	createUser($file);
-
-	//echo json_encode("working");
-
-    //codigo de PHP hago lo que sea y al final necesito
-    //enviar el siguiente array
-
-    //envio del resultado imprimiendolo: variable PHP a JSON
-	
-	//echo searchUser($name, $password);
-?>
+//envio del resultado imprimiendolo: variable PHP a JSON
+//echo searchUser($name, $password);
