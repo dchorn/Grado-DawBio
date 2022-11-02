afec35e08bc46909020c2e30b9c238f9cd688443
Denys Chorny <denis.man.07@gmail.com>
2022-11-02 19:58:02 +0100

    php db

---
+++ JavaScript/Practicas/Pt1/php/durationPrice.php
@@ -0,0 +1,3 @@
+<?php
+echo json_encode([["origen" => "Barcelona", "Destino" => "Madrid", "duracion" => "1h 7min", "precio" => "55 euros"], ["origen" => "Barcelona", "Destino" => "Valencia", "duracion" => "57min", "precio" => "40 euros"], ["origen" => "Madrid", "Destino" => "Valencia", "duracion" => "52min", "precio" => "35 euros"]])
+?>
---
+++ JavaScript/Practicas/Pt1/php/hours.php
@@ -0,0 +1,3 @@
+<?php
+echo json_encode([["day" => "Dilluns", "time" => ["8:00","17:00"]], ["day" => "Divendres", "time" => ["7:00","19:30"]], ["day" => "Dissabte", "time" => ["14:00"]]])
+?>
